491ae55010e4a1bdc9f2f7c2d76e616be9814036
renhelove <514604870@qq.com>
2018-01-05 17:20:55 +0800

    qq

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>project</title>
+	<script src="//cdn.bootcss.com/jquery/3.0.0/jquery.min.js"></script>
+	<link href="//cdn.bootcss.com/bootstrap/3.0.0/css/bootstrap-theme.min.css" rel="stylesheet">
+	<link href="//cdn.bootcss.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+	<script src="//cdn.bootcss.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+	<div class="jumbotron">
+		<h1>Hello, project!</h1>
+		<p><a class="btn btn-primary btn-lg" href="library/index.php" role="button">library</a></p>
+	</div>
+</div>
+</body>
+</html>
---
+++ library/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/5
+ * Time: 15:39
+ */
+echo 111;
